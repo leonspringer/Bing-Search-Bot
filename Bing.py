f413f80f168e1416e88a33ba6d8063683a1fa6a5
kaptkrunch <leon.springer@gmail.com>
2015-04-07 18:34:25 -0700

    added parameters and fixed bug for max searches triggering when pc searches were greater than mobile

--- Bing.py
+++ Bing.py
@@ -1,48 +1,37 @@
-#!/usr/bin/env python
-# -*-coding:utf-8-*-
-
-
-# Bing Search Bot
-# V1.0
-# Last Modified: 3/12/2015
-# 
-# This bot uses the selenium library to search for items using bing.
-# Accumulates rewards points for bing rewards.
-# Web browsers can only get 15 points a day.
-# Opens in a firefox window, you can see it work there.
-# 
-# How to use:
-#   - Install selenium using pip 'pip install -U selenium'
-#   - Register an email with live.com and with bing rewards prior to using the bot
-#   - Run and enjoy the benefits
-#
-# Edit email and password to appropriate login
-# 
-# V1.1 Updates:
-# - Handles if current weekday is tuesday of may, which means more points!
-# V1.2 Updates:
-# - Removed link clinking, switched to chromedriver in preparation for mobile search update.
-# V1.3 Updates:
-# - Added the abilty to have email and password outside of file in accounts.txt
-# V1.4 Updates:
-# - Added mobile search emulation! 
-# - Moved search options for future parameterization.
-
 import os,sys,random,time,datetime
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
+import argparse
 
 #--- Options
-normal_searches_pc = 30
+normal_searches_pc = 31
 most_searches_pc = normal_searches_pc * 2
 
-normal_searches_mobile = 20
+normal_searches_mobile = 21
 most_searches_mobile = normal_searches_mobile * 2
 
+#--- default arguments
 accounts_location = './accounts.txt'
 dictionary_location = './dictionary.txt'
 chromedriver_location = './chromedriver'
+
+
+parser = argparse.ArgumentParser(description='This bot uses the selenium library to search for items using bing.')
+parser.add_argument('-a','--accounts', dest="accounts_location", default = accounts_location, help='')
+parser.add_argument('-d','--dictionary', dest="dictionary_location", default = dictionary_location, help='')
+parser.add_argument('-c','--chrome-driver', dest="chromedriver_location", default = chromedriver_location, help='')
+parser.add_argument('-p','--pc-searches', dest="normal_searches_pc", type = int, default = normal_searches_pc, help='')
+parser.add_argument('-m','--mobile-searches', dest="normal_searches_mobile", type = int, default = normal_searches_mobile, help='')
+args = parser.parse_args()
+
+accounts_location = args.accounts_location
+dictionary_location = args.dictionary_location
+chromedriver_location = args.chromedriver_location
+normal_searches_pc = args.normal_searches_pc
+normal_searches_mobile = args.normal_searches_mobile
+most_searches_pc = args.normal_searches_pc * 2
+most_searches_mobile = args.normal_searches_mobile * 2
 
 #--- Use email@somewhere.com:P@ssWord format for each line in accounts.txt
 with open(accounts_location) as f:
@@ -51,6 +40,9 @@ with open(accounts_location) as f:
 #--- Dictionary stored here
 with open(dictionary_location) as f:
   search_terms = f.readlines()
+
+
+
 
 
 
@@ -64,16 +56,21 @@ class account(object):
     self.page_searches = 0
     
   def desktop(self):
-    print ('Running desktop searches...')
     #--- Determine max searches:
     now = datetime.datetime.now()
     if now.strftime('%A') == 'Tuesday' and now.strftime('%B') == 'May':
-      print 'Day of week: ',now.strftime('%A')
-      print 'Every Tuesday in May gets up to 30 searches rewarded.' 
+      print ("Running "  + str(most_searches_pc) +  " desktop searches...")
+      if most_searches_pc == 0:
+        return
+      print ('Day of week: ',now.strftime('%A'))
+      print ('Every Tuesday in May gets up to 30 searches rewarded.')
       max_searches_pc = most_searches_pc
     else:
-      print 'Day of week: ',now.strftime('%A')
-      print 'Up to 15 searches rewarded today.'
+      print ("Running "  + str(normal_searches_pc) +  " desktop searches...")  
+      if normal_searches_pc == 0:
+        return
+      print ('Day of week: ',now.strftime('%A'))
+      print ('Up to 15 searches rewarded today.')
       max_searches_pc = normal_searches_pc
     
     #--- Create instance of Chrome
@@ -87,7 +84,7 @@ class account(object):
     password = driver.find_element_by_id("i0118")
     
     #--- Enter login details and submit
-    print 'Account: ', self.email
+    print ('Account: ', self.email)
     email.send_keys(self.email)
     password.send_keys(self.password)
     password.send_keys(Keys.ENTER)
@@ -102,7 +99,7 @@ class account(object):
     while(repeat):
       #--- Print current point count
       rewards = driver.find_element_by_id('id_rc')
-      print 'Current Points: ',rewards.text
+      print ('Current Points: ',rewards.text)
 
       #--- Find the search bar
       searchbar = driver.find_element_by_id('sb_form_q')
@@ -113,7 +110,7 @@ class account(object):
             
       #--- Generate a random term to search for:
       term = search_terms[random.randrange(0,len(search_terms))]
-      print 'Searching for:\t', term
+      print ('Searching for:\t', term)
       
       try:
         #--- Enter the search term:
@@ -130,29 +127,31 @@ class account(object):
           #--- Return to previous screen
           #driver.back()
         except:
-          print "No Links"
+          print ("No Links")
     
       except:
-        print 'Problem searching for the term'
+        print ('Problem searching for the term')
       
       #--- Quit if we've done enough searches
       if self.page_searches >= max_searches_pc:
-        print "Maximum searches of " + str(max_searches_pc) + " reached, exiting.\n"
+        print ("Maximum searches of " + str(max_searches_pc) + " reached, exiting.\n")
+        self.page_searches = 0
         driver.quit()
         break
 
 
   def mobile(self):
-    print ('Running mobile searches...')
     #--- Determine max searches:
     now = datetime.datetime.now()
     if now.strftime('%A') == 'Tuesday' and now.strftime('%B') == 'May':
-      print 'Day of week: ',now.strftime('%A')
-      print 'Every Tuesday in May gets up to 20 searches rewarded.' 
+      print ("Running "  + str(most_searches_pc) +  " mobile searches...")
+      print ('Day of week: ',now.strftime('%A'))
+      print ('Every Tuesday in May gets up to 20 searches rewarded.')
       max_searches_mobile = most_searches_mobile
     else:
-      print 'Day of week: ',now.strftime('%A')
-      print 'Up to 10 searches rewarded today.'
+      print ("Running "  + str(normal_searches_mobile) +  " mobile searches...")  
+      print ('Day of week: ',now.strftime('%A'))
+      print ('Up to 10 searches rewarded today.')
       max_searches_mobile = normal_searches_mobile
     
     #--- Create instance of Chrome
@@ -169,7 +168,7 @@ class account(object):
     password = driver.find_element_by_id("i0118")
     
     #--- Enter login details and submit
-    print 'Account: ', self.email
+    print ('Account: ', self.email)
     email.send_keys(self.email)
     password.send_keys(self.password)
     password.send_keys(Keys.ENTER)
@@ -184,7 +183,7 @@ class account(object):
     while(repeat):
       #--- Print current point count
       rewards = driver.find_element_by_id('id_rc')
-      print 'Current Points: ',rewards.text
+      print ('Current Points: ',rewards.text)
 
       #--- Find the search bar
       searchbar = driver.find_element_by_id('sb_form_q')
@@ -195,7 +194,7 @@ class account(object):
             
       #--- Generate a random term to search for:
       term = search_terms[random.randrange(0,len(search_terms))]
-      print 'Searching for:\t', term
+      print ('Searching for:\t', term)
       
       try:
         #--- Enter the search term:
@@ -212,14 +211,17 @@ class account(object):
           #--- Return to previous screen
           #driver.back()
         except:
-          print "No Links"
+          print ("No Links")
     
       except:
-        print 'Problem searching for the term'
+        print ('Problem searching for the term')
       
       #--- Quit if we've done enough searches
       if self.page_searches >= max_searches_mobile:
-        print "Maximum searches of " + str(max_searches_mobile) + " reached, exiting.\n"
+        rewards = driver.find_element_by_id('id_rc')
+        print ('Current Points: ',rewards.text)        
+        print ("Maximum searches of " + str(max_searches_mobile) + " reached, exiting.\n")
+        self.page_searches = 0
         driver.quit()
         break
 
